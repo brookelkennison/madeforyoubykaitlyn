3441e6d675d2329bb87cc01f7137963c15662f1d
Brooke Kennison <brookelkennison@gmail.com>
2020-09-19 23:58:28 -0400

    added react router for page views

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
 import logo from '../assets/logo.png';
 import cartIcon from '../assets/cart.png';
 
 const Nav = () => {
     return ( 
-        <div className="Nav">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="navLinks">
-                <a href="home">Home</a>
-                <a href="shop">Shop</a>
-                <a href="aboutme">About Me</a>
-                <a href="contact">Contact</a>
-                <a href="mywork">My Work</a>
+        <Router>
+            <div className="Nav">
+                <img src={logo} id="logo" alt="logo"/>
+                <div id="navLinks">
+                    <Link to="/">Home</Link>
+                    <Link to="/shop">Shop</Link>
+                    <Link to="/about-me">About Me</Link>
+                    <Link to="/contact">Contact</Link>
+                    <Link to="/my-work">My Work</Link>
+                </div>
+                <img src={cartIcon} id="cartIcon" alt="cart"/>
             </div>
-            <img src={cartIcon} id="cartIcon" alt="cart"/>
-        </div>
+        </Router>
      );
 }
  
