f00e6425f3a51245801e55033f157ee8112238a4
Brooke Kennison <brookelkennison@gmail.com>
2020-11-10 17:09:44 -0500

    fix bind for handle submit

--- client/src/components/Store.js
+++ client/src/components/Store.js
@@ -1,88 +1,111 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class Store extends Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
             products: [],
-            productName: '',
+            productName: "",
             productPrice: 0,
-            productImage: ''
-        }
-    };
-    getProducts = () => {
-        fetch('/products')
-        .then(res => res.json())
-        .then(products => this.setState({products}))
+            productImage: "",
+        };
     }
+    getProducts = () => {
+        fetch("/products")
+            .then((res) => res.json())
+            .then((products) => this.setState({ products }));
+    };
     componentDidMount() {
-        this.getProducts()
+        this.getProducts();
     }
     handleChange = (event, inputField) => {
-        console.log('hi')
+        console.log("hi");
         this.setState({
-            [inputField]: event.target.value
-        })
+            [inputField]: event.target.value,
+        });
     };
     // needs work
     handleSubmit = (event) => {
-        event.preventDefault()
-        const payload = {
-            productName: this.state.productName,
-            productPrice: this.state.productPrice,
-            productImage: this.state.productImage,
-        }
-        fetch('/products', {
-            method: 'POST',
-            body: {
-                payload
-            }
-        })
-        .then(() => {
-            console.log('Data has been sent to the server')
-        })
-        .catch(() => {
-            console.log('Internal server error')
-        })
-
-    }
+        // const payload = {
+        //     productName: this.productName,
+        //     productPrice: this.productPrice,
+        //     productImage: this.productImage,
+        // };
+        // fetch("/products", {
+        //     method: "POST",
+        //     body: {
+        //         payload,
+        //     },
+        // })
+        //     .then(() => {
+        //         console.log("Data has been sent to the server");
+        //     })
+        //     .catch(() => {
+        //         console.log("Internal server error");
+        //     });
+        event.preventDefault();
+        console.log("Handle Submit");
+    };
 
     render() {
         // const products = this.state
         return (
             <div>
                 <h1>Store</h1>
-                <form onSubmit={this.handleSubmit} >
+                <form onSubmit={this.handleSubmit}>
                     <label>Product Name:</label>
-                    <input type="text" name="productName" value={this.state.productName} onChange={event => this.handleChange(event, 'productName')} />
+                    <input
+                        type="text"
+                        name="productName"
+                        value={this.state.productName}
+                        onChange={(event) =>
+                            this.handleChange(event, "productName")
+                        }
+                    />
                     <label>Product Price:</label>
-                    $<input type="number" name="productPrice" value={this.state.productPrice} onChange={event => this.handleChange(event, 'productPrice')} />
+                    $
+                    <input
+                        type="number"
+                        name="productPrice"
+                        value={this.state.productPrice}
+                        onChange={(event) =>
+                            this.handleChange(event, "productPrice")
+                        }
+                    />
                     <label>Product Image:</label>
-                    <input type="file" name="productImage" accept="image/*" value={this.state.productImage} onChange={event => this.handleChange(event, 'productImage')} />
+                    <input
+                        type="file"
+                        name="productImage"
+                        accept="image/*"
+                        value={this.state.productImage}
+                        onChange={(event) =>
+                            this.handleChange(event, "productImage")
+                        }
+                    />
                     <input type="submit" value="Submit" />
                 </form>
                 <h1>Products</h1>
-                { this.state.products.map(product =>
+                {this.state.products.map((product) => (
                     <div key={product._id}>
                         <h2>{product.productName}</h2>
                         <p>${product.productPrice}.00</p>
                         {/* <img href={}/> */}
-                        <button 
+                        <button
                             className="snipcart-add-item"
                             data-item-name={product.productName}
                             data-item-price={product.productPrice}
                             data-item-id={product._id}
                             data-item-image={product.productImage}
-                        >Add to Cart</button>
+                        >
+                            Add to Cart
+                        </button>
                     </div>
-                )}
+                ))}
             </div>
-        )
+        );
     }
 }
-
 
 export default Store;
